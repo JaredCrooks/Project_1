262c15ccaedc103b020cab95a497ba662ed24194
Jared Crooks <c3329666@uon.edu.au>
2019-04-03 22:46:15 +0000

    Added an input file

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main {
-    //code here
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d %d %d", a, b, c);
     return 0;
 }
