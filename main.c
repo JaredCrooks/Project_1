95d16426849e62bcbdd256166d0c6b03fe80c763
Jared Crooks <c3329666@uon.edu.au>
2019-04-04 05:15:22 +0000

    The rotation encryption was moved to a fuction Decryption was also added as a fuction.

--- main.c
+++ main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void encryption(char *msg, int K); //function prototypes 
+void encryption(char *alpha, int K, int Z); //function prototypes 
+void Dcryption(char *alpha, int K, int Z);
 void zero(char *x, int N);
 
 /*------------MAIN CODE STARTS HERE------------MAIN CODE STARTS HERE------------MAIN CODE STARTS HERE------------*/
 
 int main() {
+    int K = 5; // the Key (rotation amount)
     int Z = 26; /* length of the message YET TO COMPLETE: auto find legth*/
-    char alpha[Z]; //set up array to store message
-    zero(alpha, Z); // zero the array 
-    
-    for(int i = 0; i < Z; i++){ // runs for the length of the message
-        scanf("%c", &alpha[i]); //taking values from stdin and storing them in aphla
-        if(alpha[i] >= 97 && alpha[i] <= 122){// if lowercase make uppercase
-            alpha[i]= alpha[i] - 32;      
-       }
-    }
+   
+    char msg[Z]; //set up array to store message
+    zero(msg, Z); // zero the array 
+    encryption(msg, K, Z);
+    Dcryption(msg, K, Z);
     
     for(int i =0; i < Z; i++){
-        printf("%d\n", (int)alpha[i]); //print the array
+        printf("%c", (int)msg[i]); //print the array CHANGE TO %c FOR LETTER, %d FOR ASCII
     }
 
     return 0;
@@ -28,8 +26,37 @@ int main() {
 /*------------MAIN CODE ENDS HERE------------MAIN CODE ENDS HERE------------MAIN CODE ENDS HERE------------*/
 
 
-void encryption(char *str, int K){ //YET TO COMPLETE: rewirte encyrption code into this function
-    
+void encryption(char *alpha, int K, int Z){ //YET TO COMPLETE: rewirte encyrption code into this function
+    for(int i = 0; i < Z; i++){ // runs for the length of the message
+        scanf("%c", &alpha[i]); //taking values from stdin and storing them in aphla
+        if(alpha[i] >= 97 && alpha[i] <= 122){// if lowercase make uppercase
+            alpha[i]= alpha[i] - 32;      
+       }
+       alpha[i]= alpha[i] + K;
+       if(alpha[i] > 90){
+           alpha[i] = alpha[i] - 26;
+       }
+       if(alpha[i] < 65){
+           alpha[i] = alpha[i] + 26;
+       }
+
+    }
+}
+void Dcryption(char *alpha, int K, int Z){ //YET TO COMPLETE: rewirte encyrption code into this function
+    for(int i = 0; i < Z; i++){ // runs for the length of the message
+        scanf("%c", &alpha[i]); //taking values from stdin and storing them in aphla
+        if(alpha[i] >= 97 && alpha[i] <= 122){// if lowercase make uppercase
+            alpha[i]= alpha[i] - 32;      
+       }
+       alpha[i]= alpha[i] - K;
+       if(alpha[i] > 90){
+           alpha[i] = alpha[i] - 26;
+       }
+       if(alpha[i] < 65){
+           alpha[i] = alpha[i] + 26;
+       }
+
+    }
 }
 void zero(char *x, int N){ // function to set all values of an array to zero 
     int i =0;
