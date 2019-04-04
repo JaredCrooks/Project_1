360759aaf3e08351eadc838cb4ffcf42e3627f93
Jared Crooks <c3329666@uon.edu.au>
2019-04-04 04:36:41 +0000

    Outputs given ascii values

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+void encryption(char *msg, int K);
+void zero(char *x, int N);
 
 int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    printf("%d %d %d", a, b, c);
+    char alpha[4];
+    zero(alpha, 4);
+    
+    for(int i = 0; i < 4; i++){
+        scanf("%c", &alpha[i]);
+       
+    }
+    
+    for(int i =0; i < 4; i++){
+        printf("%d\n", (int)alpha[i]);
+    }
+
     return 0;
+}
+
+void encryption(char *str, int K){
+    
+}
+void zero(char *x, int N){
+    int i =0;
+    for(i= 0; i < N; i++){
+    x[i] = 0;
+    }
 }
