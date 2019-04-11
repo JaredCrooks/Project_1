0ceadaf01a2f641cd03a17bc0cd5c57746c953e3
Jared Crooks <c3329666@uon.edu.au>
2019-04-11 06:47:43 +0000

    Need to work on decryption

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ TODOO Run Program and be greeted by an user interface
 void encryption1(char *alpha, int K, int Z); 
 void Dcryption1(char *alpha, int K, int Z);
 void encryption2(char *alpha, int Z);
+void decryption2(char *alpha, int Z);
 void zero(char *x, int N);
 
 /*------------MAIN CODE STARTS HERE------------MAIN CODE STARTS HERE------------MAIN CODE STARTS HERE------------*/
@@ -30,8 +31,8 @@ int main() {
     
     //encryption1(msg, K, Z);  //takes the array, the value of the key and the length of the array
     //Dcryption1(msg, K, Z);   //takes the array, the value of the key and the length of the array
-    encryption2(msg, Z);
-
+    //encryption2(msg, Z);
+    decryption2(msg, Z);
     
     for(int i =0; i < Z; i++){
         printf("%c", (int)msg[i]); //print the array CHANGE TO %c FOR LETTER, %d FOR ASCII
@@ -108,7 +109,7 @@ void Dcryption1(char *alpha, int K, int Z){
 
 
 /* ---------------------------------------------------------------
-Substituion cyher
+Substituion cyher encryption
 
 This function takes an array letters, and the length of the array 
  and forces the letters all to uppercase. 
@@ -153,6 +154,51 @@ void encryption2(char *alpha, int Z){
     }
 }
 
+/* ---------------------------------------------------------------
+Substituion cyher DECRYPTION
+
+This function takes an array letters, and the length of the array 
+ and forces the letters all to uppercase. 
+-----------------------------------------------------------------*/
+
+void decryption2(char *alpha, int Z){ 
+    
+    
+    
+    
+    FILE *key;
+    key = fopen("Key.txt", "r");
+    char CODE[26];                      // array to store Key code
+    for(int i =0; i < 26; i++){
+        if(!feof(key) && i < 26){                 //while not at end of file
+            CODE[i] = (char)fgetc(key);//take value from file Key and put into array CODE
+        } 
+        
+        if(CODE[i] >= 97 && CODE[i] <= 122){// if lowercase make uppercase
+            CODE[i]= CODE[i] - 32;      //performing math on ascii values to make uppercase
+        }         
+    }
+    
+    
+    FILE *msg;
+    msg = fopen("Message", "r");                    
+    for(int i = 0; i < Z; i++){         // runs for the length of the message
+        
+        if(!feof(msg)){                 //while not at end of file
+            alpha[i] = (char)fgetc(msg);//take value from file message and put into array alpha
+        }
+        
+        if(alpha[i] >= 97 && alpha[i] <= 122){// if lowercase make uppercase
+            alpha[i]= alpha[i] - 32;      //performing math on ascii values to make uppercase
+        }
+        
+        if(alpha[i] > 64 && alpha[i] < 91){ //preventing non letters from being changed 
+            // Main code to encyrpt goes here
+            char tmp = alpha[i] - 65;
+            alpha[i] = CODE[(int)tmp];
+        }
+    }
+}
 /* ---------------------------------------------------------------
 function to set all values of an array to zero 
 
